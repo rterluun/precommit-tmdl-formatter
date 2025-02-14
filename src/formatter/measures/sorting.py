1bdb726bf4a0644c827f86e5ab53eab29a3da822
Robert ter Luun <rterluun@ilionx.com>
2025-02-14 14:40:32 +0100

    Implement measure extraction, sorting, and replacement functions with corresponding tests

---
+++ src/formatter/measures/sorting.py
@@ -0,0 +1,24 @@
+from re import DOTALL
+from re import compile as re_compile
+from re import search as re_search
+from typing import List
+
+MEASURE_REGEX = re_compile(r"(\s+measure\s.*?lineageTag: .+?\n)", DOTALL)
+
+
+def extract_measures(content: str) -> List[str]:
+
+    measures: List[str] = MEASURE_REGEX.findall(content)
+    return measures
+
+
+def sort_measures(measures: List[str]) -> List[str]:
+
+    measures.sort(key=lambda x: re_search(r"measure\s+([^=]+)", x).group(1).lower())
+    return measures
+
+
+def replace_measures(content: str, measures: List[str]) -> str:
+
+    new_content = MEASURE_REGEX.sub(lambda _: measures.pop(0), content)
+    return new_content
