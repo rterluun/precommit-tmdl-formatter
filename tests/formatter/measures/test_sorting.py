bbf1e24d39b3788eaa1cd4e3109d10e916834d18
Robert ter Luun <rterluun@ilionx.com>
2025-02-14 15:09:49 +0100

    Rename formatter package to tmdl_formatter and migrate sorting functions with corresponding tests

--- tests/formatter/measures/test_sorting.py
+++ tests/formatter/measures/test_sorting.py
@@ -1,10 +1,6 @@
 from typing import List
 
-from src.formatter.measures.sorting import (
-    extract_measures,
-    replace_measures,
-    sort_measures,
-)
+from src.tmdl_formatter.measures.sorting import extract_measures, replace_measures, sort_measures
 from tests.conftest import MEASURES_SORTED_IN_STRING
 
 
@@ -12,9 +8,7 @@ def test_extract_measures(measures_in_string: str, unsorted_measures_in_list):
     assert extract_measures(content=measures_in_string) == unsorted_measures_in_list
 
 
-def test_sort_measures(
-    unsorted_measures_in_list: List[str], sorted_measures_in_list: List[str]
-):
+def test_sort_measures(unsorted_measures_in_list: List[str], sorted_measures_in_list: List[str]):
 
     assert sort_measures(measures=unsorted_measures_in_list) == sorted_measures_in_list
 
