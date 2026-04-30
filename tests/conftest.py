8fb8aa48d59c327b0c2a3bb4ffc35125e82a5ca0
rterluun <128294216+rterluun@users.noreply.github.com>
2026-04-30 18:16:58 +0200

    Release/v0.1.1 (#7)
    * Fix regex pattern for measure extraction
    
    Fix: keep /// doc comments anchored to their measure when sorting

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,6 +33,12 @@ table _measures
         formatString: #,0
         displayFolder: Folder1\Folder2
         lineageTag: 65e1c794-4323-4801-b404-a320bf7f5abb
+
+    /// This is a comment about the measure below
+    measure 'Measure f' = [field1]-[field2]
+        formatString: #,0
+        displayFolder: Folder1\Folder2
+        lineageTag: 65e1c794-4323-4801-b404-a320bf7f5abb
     """
 
 
@@ -48,6 +54,12 @@ table _measures
         lineageTag: 65e1c794-4323-4801-b808-a320bf7f5abb
 
         annotation PBI_FormatHint = {"isGeneralNumber":false}
+
+    /// This is a comment about the measure below
+    measure 'Measure f' = [field1]-[field2]
+        formatString: #,0
+        displayFolder: Folder1\\Folder2
+        lineageTag: 65e1c794-4323-4801-b404-a320bf7f5abb
 
     measure 'Measure c' = [field1]-[field2]
         formatString: #,0
@@ -77,6 +89,7 @@ table _measures
 def unsorted_measures_in_list():
     return [
         "\n\n    measure 'Measure a' = [field1]-[field2]\n        formatString: #,0\n        displayFolder: Folder1\\Folder2\n        lineageTag: 65e1c794-4323-4801-b808-a320bf7f5abb\n\n        annotation PBI_FormatHint = {\"isGeneralNumber\":false}\n",
+        "\n    /// This is a comment about the measure below\n    measure 'Measure f' = [field1]-[field2]\n        formatString: #,0\n        displayFolder: Folder1\\Folder2\n        lineageTag: 65e1c794-4323-4801-b404-a320bf7f5abb\n",
         "\n    measure 'Measure c' = [field1]-[field2]\n        formatString: #,0\n        displayFolder: Folder1\\Folder2\n        lineageTag: 65e1c794-4323-4801-b404-a320bf7f5abb\n",
         "\n    measure 'Measure b' = SUM('field1'[Count])\n        formatString: #,0\n        displayFolder: Folder1\\Folder2\n        lineageTag: 357ac151-baaf-47c6-92ff-033eeacdeb60\n\n        annotation PBI_FormatHint = {\"isGeneralNumber\":true}\n",
         "\n    measure 'Measure e' = [field1]-[field2]\n        formatString: #,0\n        displayFolder: Folder1\\Folder2\n        lineageTag: 65e1c794-4323-4801-b404-a320bf7f5abb\n",
@@ -92,4 +105,5 @@ def sorted_measures_in_list():
         "\n    measure 'Measure c' = [field1]-[field2]\n        formatString: #,0\n        displayFolder: Folder1\\Folder2\n        lineageTag: 65e1c794-4323-4801-b404-a320bf7f5abb\n",
         "\n    measure Measure d = [field1]-[field2]\n        formatString: #,0\n        displayFolder: Folder1\\Folder2\n        lineageTag: 65e1c794-4323-4801-b404-a320bf7f5abb\n",
         "\n    measure 'Measure e' = [field1]-[field2]\n        formatString: #,0\n        displayFolder: Folder1\\Folder2\n        lineageTag: 65e1c794-4323-4801-b404-a320bf7f5abb\n",
+        "\n    /// This is a comment about the measure below\n    measure 'Measure f' = [field1]-[field2]\n        formatString: #,0\n        displayFolder: Folder1\\Folder2\n        lineageTag: 65e1c794-4323-4801-b404-a320bf7f5abb\n",
     ]
